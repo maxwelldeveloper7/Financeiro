0be1110f0ff20f4ff8cca152bcf1ef712aa3eeba
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2020-09-04 13:45:17 -0300

    titulo e rodapé

--- menu.py
+++ menu.py
@@ -1,1 +1,8 @@
-print("Sistema de Gestão Financeira")
+opcao = -1
+titulo_ = "---|Sistema de Gestão Financeira|---"
+rodape = "------------------------------------"
+print(titulo_)
+print()
+print("Escolha uma opção:")
+print()
+print(rodape)
