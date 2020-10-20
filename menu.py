f192da85b82b938f84914cf7ceb44c5a9d77cbf7
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2020-10-20 13:24:56 -0300

    telas de lançamentos e cadastros

--- menu.py
+++ menu.py
@@ -4,14 +4,42 @@ import os
 
 opcao = -1
 while True:
+	#variável que será usada para controlar o loop interno dos menus
+	op = -1
 	tela_inicial()
+	
 	try:
 		opcao = int(input("Escolha uma opção: "))
 		os.system('clear') or None
 	except ValueError:
-		print("Opção inválida!")		
+		print("\nOpção inválida!\n")		
 	
 	if opcao == 0:
 		break
+	
+	if opcao == 1:
+		while True:
+			os.system('clear') or None
+			tela_cadastros()
+			try:
+				op = int(input("Escolha uma opção: "))
+			except ValueError:
+				print("\nOpção inválida!\n")
+			if op == 0:
+				os.system('clear') or None
+				break
+	
+	if opcao == 2:
+		while True:
+			os.system('clear') or None
+			tela_lancamentos()
+			try:
+				op = int(input("Escolha uma opção: "))
+			except ValueError:
+				print("\nOpção inválida!\n")
+			if op == 0:
+				os.system('clear') or None
+				break
+			
 
 print("Você saiu da aplicação, até mais!")
--- telas.py
+++ telas.py
@@ -1,6 +1,7 @@
 import os
 
 def tela_inicial():	
+	
 	tela = '''\
 «««««|Sistema de Gestão Financeira|»»»»»
 
@@ -8,5 +9,36 @@ def tela_inicial():
  2 » Lançamentos
  3 » Relátorios
  0 « Sair
+ '''
+	print(tela)
+
+def tela_cadastros():	
+	
+	tela = '''\
+«««««|Cadastros|»»»»»
+
+ 1 » Proventos e/ou Serviços Prestados
+ 2 » Clientes
+ 3 » Despesas
+ 4 » Doações
+ 5 » Contas
+ 0 « Voltar
+ '''
+	print(tela)
+
+def tela_lancamentos():	
+	
+	tela = '''\
+«««««|Gegistro de Lançamentos|»»»»»
+
+ 1 » Proventos a Receber
+ 2 » Proventos Recebidos
+ 3 » Pagamentos de Clientes a Receber
+ 4 » Pagamentos de Clientes Recebidos
+ 5 » Dízimos e Ofertas a Devolver
+ 6 » Despesas a Pagar
+ 7 » Despesas pagas
+ 8 » Transferência de Valores entre Contas
+ 0 « Voltar
  '''
 	print(tela)
