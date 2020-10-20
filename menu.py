73d6a07c900c879fce4c32e592ce86cd8b747382
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2020-10-20 11:15:34 -0300

    trabalhando no loop do menu inicial

--- menu.py
+++ menu.py
@@ -3,11 +3,15 @@ from telas import *
 import os
 
 opcao = -1
-while opcao != 0:
+while True:
 	tela_inicial()
 	try:
 		opcao = int(input("Escolha uma opção: "))
 		os.system('clear') or None
 	except ValueError:
-		
-		print("Opção inválida!")
+		print("Opção inválida!")		
+	
+	if opcao == 0:
+		break
+
+print("Você saiu da aplicação, até mais!")
