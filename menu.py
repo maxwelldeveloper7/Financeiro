f8066e5f513ca5d39ae92d8a2431cbebb76b5ce8
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2020-10-20 18:35:49 -0300

    otimização do menu

--- menu.py
+++ menu.py
@@ -2,44 +2,60 @@
 from telas import *
 import os
 
-opcao = -1
-while True:
-	#variável que será usada para controlar o loop interno dos menus
-	op = -1
-	tela_inicial()
-	
-	try:
-		opcao = int(input("Escolha uma opção: "))
-		os.system('clear') or None
-	except ValueError:
-		print("\nOpção inválida!\n")		
-	
-	if opcao == 0:
-		break
-	
-	if opcao == 1:
-		while True:
-			os.system('clear') or None
-			tela_cadastros()
-			try:
-				op = int(input("Escolha uma opção: "))
-			except ValueError:
-				print("\nOpção inválida!\n")
-			if op == 0:
-				os.system('clear') or None
-				break
-	
-	if opcao == 2:
-		while True:
-			os.system('clear') or None
-			tela_lancamentos()
-			try:
-				op = int(input("Escolha uma opção: "))
-			except ValueError:
-				print("\nOpção inválida!\n")
-			if op == 0:
-				os.system('clear') or None
-				break
-			
+opcoes = [0, 1, 2, 3]
 
-print("Você saiu da aplicação, até mais!")
+mensagem = "\nEscolha uma opção: "
+
+while True:
+	os.system('clear') or None
+	tela_inicial()
+	try:
+		opcao = int(input(mensagem))
+	except ValueError:
+		opcao = -1
+	
+	if opcao in opcoes:
+		mensagem = "\nEscolha uma opção: "
+		
+		if opcao == 0:
+			break
+		
+		if opcao == 1:
+			opcoes_1 = [0, 1, 2, 3, 4, 5]
+			while True:
+				os.system('clear') or None
+				tela_cadastros()
+				try:
+					op1 = int(input(mensagem))
+				except ValueError:
+					op1 = -1
+		
+				if op1 in opcoes_1:
+					mensagem = "\nEscolha uma opção: "
+					if op1 == 0:
+						break
+				else:
+					mensagem = "Opção inválida!\nEscolha uma opção: "
+		
+		if opcao == 2:
+			opcoes_2 = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+			while True:
+				os.system('clear') or None
+				tela_lancamentos()
+				try:
+					op2 = int(input(mensagem))
+				except ValueError:
+					op2 = -1
+		
+				if op2 in opcoes_2:
+					mensagem = "\nEscolha uma opção: "
+					if op2 == 0:
+						break
+				else:
+					mensagem = "Opção inválida!\nEscolha uma opção: "
+	else:
+		mensagem = "Opção inválida!\nEscolha uma opção: "
+	
+			
+os.system('clear') or None
+print("\nVocê saiu da aplicação, até mais!")
